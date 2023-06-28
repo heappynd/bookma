8ec45e14ed2d670cf7a24c22603e828a9a9b5d37
heappynd <heappynd@163.com>
2023-06-28 18:49:17 +0800

    feat: bump nuxt so storage api support generic

--- server/stores/index.ts
+++ server/stores/index.ts
@@ -1,30 +1,22 @@
-const storage = useStorage('db')
+const storage = useStorage("db");
 
 export type Mark = {
-  id: string
-  href: string
-  add_date: number
-  text: string
-  dir_id: string
-  icon: string
-}
+  id: string;
+  href: string;
+  add_date: number;
+  text: string;
+  dir_id: string;
+  icon: string;
+};
 export type Dir = {
-  id: string
-  name: string
-}
+  id: string;
+  name: string;
+};
 
 export const getMarksPromise = () => {
-  return new Promise<Mark[]>((resolve, reject) => {
-    storage.getItem('marks').then((res) => {
-      resolve((res as Mark[]) ?? [])
-    })
-  })
-}
+  return storage.getItem<Mark[]>("marks").then((res) => res ?? []);
+};
 
 export const getDirsPromise = () => {
-  return new Promise<Dir[]>((resolve, reject) => {
-    storage.getItem('dirs').then((res) => {
-      resolve((res as Dir[]) ?? [])
-    })
-  })
-}
+  return storage.getItem<Dir[]>("dirs").then((res) => res ?? []);
+};
