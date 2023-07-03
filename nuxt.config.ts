fc85a22bd5d465a0ccfde993b5001c0b58184171
heappynd <heappynd@163.com>
2023-07-03 23:10:38 +0800

    feat: add unocss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@element-plus/nuxt"],
+  modules: ["@element-plus/nuxt", "@unocss/nuxt"],
   nitro: {
     preset: 'vercel',
     storage: {
